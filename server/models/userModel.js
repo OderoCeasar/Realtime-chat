34e17814e02394f11df47e115f4249033b0fe5de
OderoCeasar <156299730+OderoCeasar@users.noreply.github.com>
2025-08-04 13:49:22 +0300

    first commit

---
+++ server/models/userModel.js
@@ -0,0 +1,64 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+
+
+const userSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+    },
+
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+
+    password: {
+        type: String,
+        required: true,
+    },
+
+    bio: {
+        type: String,
+        default: 'Available',
+    },
+
+    profilePic: {
+        type: String,
+        default: 'https://icon-library.com/images/anonymous-avatar-icon/anonymous-avatar-icon-25.jpg',
+    },
+
+    contacts: [
+        {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "User",
+        },
+    ],
+}, { timestamps: true });
+
+userSchema.pre('save', async function (next) {
+    if (this.isModified('password')) {
+        this.password = await bcrypt.hash(this.password, 12);
+    }
+    next();
+});
+
+userSchema.methods.generateAuthToken = async function () {
+    try {
+        let token = jwt.sign(
+            { id: this._id, email: this.email },
+            process.env.SECRET,
+            {
+                expiresIn: '24h',
+            }
+        );
+
+        return token;
+    } catch (error) {
+        console.log("error while generating token");
+    }
+};
+
+module.exports = mongoose.model("User", userSchema);
